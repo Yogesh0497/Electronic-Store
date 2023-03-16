bace7e98bc7a1b3f911c9e3f09b251d6c78264a3
Yogesh0497 <yogeshmusale124@gmail.com>
2023-03-16 18:43:12 +0530

    First commit

---
+++ Subtraction/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a=100;
+
+        int b=50;
+
+        int c = a-b;
+
+        System.out.println(c);
+
+    }
+
+
+
+}
